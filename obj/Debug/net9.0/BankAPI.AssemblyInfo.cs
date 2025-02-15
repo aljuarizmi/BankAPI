6ed97b0aefb5a84231587d27ae26a938eb35c850
aljuarizmi <aljuarizmi@gmail.com>
2025-02-15 18:42:59 -0500

    Se agrega paquete Microsoft.EntityFrameworkCore.Design

--- obj/Debug/net9.0/BankAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/BankAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+524735713187f509724052a0d1a0c2852107f8fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d3a23ba433a05098f44fef4c7d35143d7503e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
