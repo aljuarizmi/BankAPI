c7a98ea55a1168408bbfe0b51d36de77d28984f1
aljuarizmi <aljuarizmi@gmail.com>
2025-02-15 19:05:49 -0500

    Se agregó el contexto de la db con la sentencia dotnet ef dbcontext scaffold 'Server=JACKIE; Database=Bank;Trusted_connection=true;TrustServerCertificate=true' Microsoft.EntityFrameworkCore.SqlServer --context-dir .\Data --output-dir .\Data\BankModels . Ver referenciael-jamesaranda 'Crear modelo con EF core'

---
+++ Data/BankContext.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using BankAPI.Data.BankModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAPI.Data;
+
+public partial class BankContext : DbContext
+{
+    public BankContext()
+    {
+    }
+
+    public BankContext(DbContextOptions<BankContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Account> Accounts { get; set; }
+
+    public virtual DbSet<AccountType> AccountTypes { get; set; }
+
+    public virtual DbSet<BankTransaction> BankTransactions { get; set; }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<TransactionType> TransactionTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=JACKIE; Database=Bank;Trusted_connection=true;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Account__3214EC27B3257922");
+
+            entity.ToTable("Account");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Balance).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.ClientId).HasColumnName("ClientID");
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.AccountTypeNavigation).WithMany(p => p.Accounts)
+                .HasForeignKey(d => d.AccountType)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Account__Account__1CF15040");
+
+            entity.HasOne(d => d.Client).WithMany(p => p.Accounts)
+                .HasForeignKey(d => d.ClientId)
+                .HasConstraintName("FK__Account__ClientI__1DE57479");
+        });
+
+        modelBuilder.Entity<AccountType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AccountT__3214EC27076BA39C");
+
+            entity.ToTable("AccountType");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<BankTransaction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__BankTran__3214EC2717A289F3");
+
+            entity.ToTable("BankTransaction");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.AccountId).HasColumnName("AccountID");
+            entity.Property(e => e.Amount).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.Account).WithMany(p => p.BankTransactions)
+                .HasForeignKey(d => d.AccountId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__BankTrans__Accou__1ED998B2");
+
+            entity.HasOne(d => d.TransactionTypeNavigation).WithMany(p => p.BankTransactions)
+                .HasForeignKey(d => d.TransactionType)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__BankTrans__Trans__1FCDBCEB");
+        });
+
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Client__3214EC27E514E90E");
+
+            entity.ToTable("Client");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TransactionType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Transact__3214EC27BE9409E7");
+
+            entity.ToTable("TransactionType");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
